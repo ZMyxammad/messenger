180f4efba7e987f7f77b495880ae6a6044bca307
ZMyxammad <148261687+ZMyxammad@users.noreply.github.com>
2025-10-25 00:09:59 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,40 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: { origin: "*" },
+});
+
+app.get("/", (req, res) => {
+  res.send("Voice chat signaling server is running ✅");
+});
+
+io.on("connection", (socket) => {
+  console.log("User connected:", socket.id);
+
+  socket.on("offer", (data) => {
+    socket.broadcast.emit("offer", { from: socket.id, sdp: data });
+  });
+
+  socket.on("answer", (data) => {
+    io.to(data.to).emit("answer", { from: socket.id, sdp: data.sdp });
+  });
+
+  socket.on("candidate", (data) => {
+    io.to(data.to).emit("candidate", {
+      from: socket.id,
+      candidate: data.candidate,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => console.log("Server running on port", PORT));
 
